b81d40497b282383242ad1816478e577a60f5e93
Marta Zaorska <marta.zaorska2@gmail.com>
2025-05-24 14:05:18 +0200

    feat(astro): fix header height and update link query

--- apps/astro/src/components/ui/Button/index.ts
+++ apps/astro/src/components/ui/Button/index.ts
@@ -7,7 +7,7 @@ export const ButtonDataQuery = (name: string) => `
     theme,
     linkType,
     "href": select(
-      linkType == "internal" => internal -> slug.current,
+      linkType == "internal" => internal -> slug.current + coalesce(anchor, ""),
       linkType == "external" => external,
       linkType == "anchor" => anchor,
       "#"
